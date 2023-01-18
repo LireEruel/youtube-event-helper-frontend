7841e6e15d2c423860644cd4a0560483f9f15a1c
Seohee Jeong <jsh001505@ncsoft.com>
2023-01-18 17:14:31 +0900

    ant design 적용

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,19 @@
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
-import Link from "next/link";
-import Image from "next/image";
+import { Button, Space, DatePicker, Card } from "antd";
+import { SearchOutlined } from "@ant-design/icons";
+
 export default function Home() {
+  const onChange = () => {};
   return (
-    <>
-      <Head>
-        <title>Youtube Event Helper</title>
-        <h1 className="title">
-          Read <Link href="/posts/first-post">this page!</Link>
-        </h1>
-      </Head>
-      <main className={styles.main}>
-        <h2>hello world</h2>
-      </main>
-      <Image
-        src="/images/profile.jpg" // Route of the image file
-        height={144} // Desired size with correct aspect ratio
-        width={144} // Desired size with correct aspect ratio
-        alt="Your Name"
-      />
-    </>
+    <div style={{ padding: 100 }}>
+      <Space direction="vertical">
+        <Button type="primary">Primary Button</Button>
+        <Button type="ghost">Ghost Button</Button>
+        <DatePicker onChange={onChange} />
+        <SearchOutlined style={{ fontSize: 60, color: "green" }} />
+        <Card>
+          <p>Card content</p>
+        </Card>
+      </Space>
+    </div>
   );
 }
