98275190f7c205f23a4e851f8322fbc0036f3203
LireEruel <jsh001505@gmail.com>
2023-01-28 13:09:43 +0900

    page 이름 변경

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,7 @@ export default function App(props: AppProps) {
   return (
     <>
       <Head>
-        <title>Page title</title>
+        <title>Youtube Event Helper</title>
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
